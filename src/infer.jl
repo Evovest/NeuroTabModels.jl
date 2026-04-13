13ac231f0f63c46c26937dfd9e1fd15a51285fd5
jeremie.desgagne.bouchard <jeremie.desgagne.bouchard@gmail.com>
2026-04-12 23:59:57 -0400

    scalers

--- src/infer.jl
+++ src/infer.jl
@@ -35,34 +35,28 @@ _assemble(::Type{<:GaussianMLE}, raw_preds) = reduce(hcat, raw_preds)
 _assemble(::Type, raw_preds) = vcat([vec(p) for p in raw_preds]...)
 
 # Apply inverse link to convert from model scale to natural scale
-_inverse_link(::Type{<:LogLoss}, pred) = sigmoid.(pred)
-_inverse_link(::Type{<:Tweedie}, pred) = exp.(pred)
-_inverse_link(::Type{<:Union{MSE,MAE}}, pred) = pred
+_inverse_link(::Type{<:LogLoss}, pred, scalers) = sigmoid.(pred)
+_inverse_link(::Type{<:Tweedie}, pred, scalers) = exp.(pred)
+_inverse_link(::Type{<:Union{MSE,MAE}}, pred, scalers) = pred .* scalers[:sigma] .+ scalers[:mu]
 
-function _inverse_link(::Type{<:MLogLoss}, pred)
+function _inverse_link(::Type{<:MLogLoss}, pred, scalers)
     return Matrix(softmax(pred; dims=1)')
 end
 
-function _inverse_link(::Type{<:GaussianMLE}, pred)
+function _inverse_link(::Type{<:GaussianMLE}, pred, scalers)
     p = Matrix(pred')
-    p[:, 2] .= exp.(p[:, 2])
+    @views p[:, 1] .= p[:, 1] .* scalers[:sigma] .+ scalers[:mu]
+    @views p[:, 2] .= exp.(p[:, 2]) .* scalers[:sigma]
     return p
 end
 
-function _postprocess(::Type{L}, preds; raw::Bool=false, scalers=nothing) where {L}
+function _postprocess(::Type{L}, preds; proj::Bool=true, scalers=nothing) where {L}
     p_raw = _assemble(L, preds)
-    if raw
-        return p_raw
-    else
-        p = _inverse_link(L, p_raw)
-        if !isnothing(scalers)
-            p .= p .* scalers[:sigma] .+ scalers[:mu]
-        end
-        return p
-    end
+    proj || return p_raw
+    return _inverse_link(L, p_raw, scalers)
 end
 
-function infer(m::NeuroTabModel{L}, data; device=:cpu, raw::Bool=false) where {L}
+function infer(m::NeuroTabModel{L}, data; device=:cpu, proj::Bool=false) where {L}
     dev = _get_device(device)
     cdev = cpu_device()
     ps = dev(m.info[:ps])
@@ -83,20 +77,16 @@ function infer(m::NeuroTabModel{L}, data; device=:cpu, raw::Bool=false) where {L
         end
         push!(preds, cdev(pred))
     end
-    return _postprocess(L, preds; raw, scalers=m.info[:scalers])
+    return _postprocess(L, preds; proj, scalers=m.info[:scalers])
 end
 
-function infer(m::NeuroTabModel, data::AbstractDataFrame; device=:cpu, raw::Bool=false)
+function infer(m::NeuroTabModel, data::AbstractDataFrame; device=:cpu, proj::Bool=true)
     dinfer = get_df_loader_infer(data; feature_names=m.info[:feature_names], batchsize=2048)
-    return infer(m, dinfer; device, raw)
+    return infer(m, dinfer; device, proj)
 end
 
-function (m::NeuroTabModel)(data::AbstractDataFrame; device=:cpu)
-    return infer(m, data; device)
-end
-
-function (m::NeuroTabModel)(x::AbstractMatrix; device=:cpu)
-    return infer(m, [(x,)]; device)
+function (m::NeuroTabModel)(data::AbstractDataFrame; device=:cpu, proj::Bool=true)
+    return infer(m, data; device, proj)
 end
 
 end
