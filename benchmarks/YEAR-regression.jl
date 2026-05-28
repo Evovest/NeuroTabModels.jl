5767d0312ee0dcf2556ac297cecdf56454e3c21e
jeremie.desgagne.bouchard <jeremie.desgagne.bouchard@gmail.com>
2026-05-28 00:29:32 -0400

    up

--- benchmarks/YEAR-regression.jl
+++ benchmarks/YEAR-regression.jl
@@ -92,7 +92,7 @@ dtest = df_tot[(end-51630+1):end, :];
 arch = NeuroTabModels.ModernNCAConfig(; d_embedding=16, n_blocks=1, d_block=32, dropout=0.1f0, sample_rate=0.1)
 
 device = :gpu
-backend = :zygote
+backend = :reactant
 loss = :mse # :mse :gaussian_mle :tweedie
 # metric = :correlation # :mse :gaussian_mle :tweedie
 
